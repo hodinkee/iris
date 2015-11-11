5bbcb369d64a503ed0ebefa59670c6086de1f0e7
Jonathan Baker <jonathan@jonathanbaker.me>
2015-11-11 11:52:43 -0500

    Add test cases.

--- Tests/IrisTests.swift
+++ Tests/IrisTests.swift
@@ -9,28 +9,36 @@
 import XCTest
 @testable import Iris
 
-class IrisTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class IrisTests: XCTestCase {
+
+    // MARK: - Tests
+
+    func testImageOptionsQueryItems() {
+        XCTAssert(ImageOptions().queryItems.isEmpty, "Default ImageOptions should return empty query items.")
+
+        let queryItems = ImageOptions(format: .JPEG, width: 320, height: 200, scale: 2.0, fit: .Crop, crop: [.Faces]).queryItems
+
+        XCTAssert(queryItems.contains(NSURLQueryItem(name: "fm", value: "jpg")))
+        XCTAssert(queryItems.contains(NSURLQueryItem(name: "w", value: "320.0")))
+        XCTAssert(queryItems.contains(NSURLQueryItem(name: "h", value: "200.0")))
+        XCTAssert(queryItems.contains(NSURLQueryItem(name: "dpr", value: "2.0")))
+        XCTAssert(queryItems.contains(NSURLQueryItem(name: "fit", value: "crop")))
+        XCTAssert(queryItems.contains(NSURLQueryItem(name: "crop", value: "faces")))
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testImgixURL() {
+        let baseURL = NSURL(string: "https://www.example.com?foo=bar")
+        let passURL = NSURL(string: "https://www.example.com?foo=bar&fm=jpg")
+        XCTAssertEqual(passURL, baseURL?.imgixURL(imageOptions: ImageOptions(format: .JPEG)))
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testImgixURLWithSigningOptions() {
+        let imageOptions = ImageOptions()
+        let signingOptions = SigningOptions(host: "example.imgix.net", token: "FOO123bar")
+
+        let sourceURL1 = NSURL(string: "http://avatars.com/john-smith.png")
+        let resultURL1 = NSURL(string: "https://example.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?s=493a52f008c91416351f8b33d4883135")
+
+        XCTAssertEqual(resultURL1, sourceURL1?.imgixURL(imageOptions: imageOptions, signingOptions: signingOptions))
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
