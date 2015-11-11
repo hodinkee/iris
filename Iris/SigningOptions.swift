def9ff09d7b8b32077667a2f01cf7dc7f6a51637
Jonathan Baker <jonathan@jonathanbaker.me>
2015-11-11 11:14:47 -0500

    Add SigningOptions.

---
+++ Iris/SigningOptions.swift
@@ -0,0 +1,33 @@
+//
+//  SigningOptions.swift
+//  Iris
+//
+//  Created by Jonathan Baker on 11/11/15.
+//  Copyright © 2015 HODINKEE. All rights reserved.
+//
+
+import Foundation
+
+public struct SigningOptions: Equatable {
+
+    // MARK: - Properties
+
+    public var host: String
+
+    public var token: String
+
+    public var secure: Bool
+    
+
+    // MARK: - Initializers
+
+    public init(host: String, token: String, secure: Bool = true) {
+        self.host = host
+        self.token = token
+        self.secure = secure
+    }
+}
+
+public func ==(lhs: SigningOptions, rhs: SigningOptions) -> Bool {
+    return (lhs.host == rhs.host) && (lhs.token == rhs.token) && (lhs.secure == rhs.secure)
+}
