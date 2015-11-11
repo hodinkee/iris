1e944e2fecd807225082f95059e5be37b26e3762
Jonathan Baker <jonathan@jonathanbaker.me>
2015-11-11 10:32:54 -0500

    Add iOS framework target.

---
+++ Iris/Targets/iOS/Iris.h
@@ -0,0 +1,12 @@
+//
+//  Iris.h
+//  Iris
+//
+//  Created by Jonathan Baker on 11/11/15.
+//  Copyright © 2015 HODINKEE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double IrisVersionNumber;
+FOUNDATION_EXPORT const unsigned char IrisVersionString[];
