e00b2816bb6e345a44bd5c17cbaddbaaeb4b1456
Jonathan Baker <jonathan@jonathanbaker.me>
2015-11-12 10:06:58 -0500

    Add adjustment properties.

--- Iris/ImageOptions.swift
+++ Iris/ImageOptions.swift
@@ -40,6 +40,159 @@ public struct ImageOptions: Equatable {
         case Entropy = "entropy"
     }
 
+    // MARK: - Adjustment Properties
+
+    /**
+     Adjusts the brightness of the image.
+    
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-bri)
+    */
+    public var brightness: Int? {
+        get { return _brightness }
+        set { _brightness = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _brightness: Int?
+
+    /**
+     Adjusts the contrast of the image.
+
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-con)
+    */
+    public var contrast: Int? {
+        get { return _contrast }
+        set { _contrast = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _contrast: Int?
+
+    /**
+     Adjusts the exposure of the image.
+
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-exp)
+    */
+    public var exposure: Int? {
+        get { return _exposure }
+        set { _exposure = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _exposure: Int?
+
+    /**
+     Adjusts gamma/midtone brightness.
+
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-gam)
+    */
+    public var gamma: Int? {
+        get { return _gamma }
+        set { _gamma = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _gamma: Int?
+
+    /**
+     Adjusts the highlight tonal mapping of an image while preserving 
+     spatial detail.
+
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-high)
+    */
+    public var highlight: Int? {
+        get { return _highlight }
+        set { _highlight = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _highlight: Int?
+
+    /**
+     Changes the overall hue, or tint, of the source pixels.
+
+     Values are clamped to the range `0...359`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-hue)
+    */
+    public var hue: Int? {
+        get { return _hue }
+        set { _hue = newValue?.clamp(min: 0, max: 359) }
+    }
+    private var _hue: Int?
+
+    /**
+     Inverts all the pixel colors and brightness values within the image
+     producing a negative of the image.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-invert)
+    */
+    public var invert: Bool?
+
+    /**
+     Adjusts the saturation of the image.
+
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-sat)
+    */
+    public var saturation: Int? {
+        get { return _saturation }
+        set { _saturation = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _saturation: Int?
+
+    /**
+     Adjusts the shadow tonal mapping of an image while preserving 
+     spatial detail.
+
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-shad)
+    */
+    public var shadow: Int? {
+        get { return _shadow }
+        set { _shadow = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _shadow: Int?
+
+    /**
+     Sharpens the image details using luminance sharpening.
+
+     Values are clamped to the range `0...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-sharp)
+    */
+    public var sharpen: Int? {
+        get { return _sharpen }
+        set { _sharpen = newValue?.clamp(min: 0, max: 100) }
+    }
+    private var _sharpen: Int?
+
+    /**
+     Adjusts the vibrance of an image while keeping pleasing skin tones.
+
+     Values are clamped to the range `-100...100`.
+
+     - seealso:
+     [Imgix API Reference](https://www.imgix.com/docs/reference/adjustment#param-vib)
+    */
+    public var vibrance: Int? {
+        get { return _vibrance }
+        set { _vibrance = newValue?.clamp(min: -100, max: 100) }
+    }
+    private var _vibrance: Int?
+
 
     // MARK: - Size Properties
 
@@ -171,6 +324,52 @@ public struct ImageOptions: Equatable {
     */
     public var queryItems: [NSURLQueryItem] {
         var items = [NSURLQueryItem]()
+
+        // Adjustment Properties
+
+        if let value = brightness {
+            items.append(NSURLQueryItem(name: "bri", value: String(value)))
+        }
+
+        if let value = contrast {
+            items.append(NSURLQueryItem(name: "con", value: String(value)))
+        }
+
+        if let value = exposure {
+            items.append(NSURLQueryItem(name: "exp", value: String(value)))
+        }
+
+        if let value = gamma {
+            items.append(NSURLQueryItem(name: "gam", value: String(value)))
+        }
+
+        if let value = highlight {
+            items.append(NSURLQueryItem(name: "high", value: String(value)))
+        }
+
+        if let value = hue {
+            items.append(NSURLQueryItem(name: "hue", value: String(value)))
+        }
+
+        if let value = invert {
+            items.append(NSURLQueryItem(name: "invert", value: String(value)))
+        }
+
+        if let value = saturation {
+            items.append(NSURLQueryItem(name: "sat", value: String(value)))
+        }
+
+        if let value = shadow {
+            items.append(NSURLQueryItem(name: "shad", value: String(value)))
+        }
+
+        if let value = sharpen {
+            items.append(NSURLQueryItem(name: "sharp", value: String(value)))
+        }
+
+        if let value = vibrance {
+            items.append(NSURLQueryItem(name: "vib", value: String(value)))
+        }
 
         // Size Properties
 
